295c9a09957ef3cb3031006a202cea6820419937
Petr Boháč <bohac3@spsejecna.cz>
2024-11-11 08:46:03 +0100

    db upd

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const JWT_SECRET = "0Wg11ndyhI+WpHVYSCT6jxrcC2OPabIcMf/4A8Ip7ug=";
 
 const db = mysql.createConnection({
-  host: "http://3.88.2.83:3050",
+  host: "3.88.2.83:3050",
   user: "root",
   password: "",
   database: "wa_api",
